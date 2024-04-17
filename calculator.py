c24623734d7b7d98e7283d9feda5c6f383c6b5b5
ac21832 <167350350+ac21832@users.noreply.github.com>
2024-04-17 19:16:54 +0200

    add substraction

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
 def sum_(a: int, b: int) -> int:
     return a + b
 
+def substract(a: int, b: int) -> int:
+    return a - b
+
 if __name__ == "__main__":
     print(sum_(10, 20))
+    print(substract(20, 10))
