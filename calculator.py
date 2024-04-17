2b5a6aeae4612ac85f8fd31ecb1636e71afb5de3
ac21832 <167350350+ac21832@users.noreply.github.com>
2024-04-17 20:26:19 +0200

    add multiplication

--- calculator.py
+++ calculator.py
@@ -1,8 +1,14 @@
 def sum_(a: int, b: int) -> int:
     return a + b
 
+
 def substract(a: int, b: int) -> int:
     return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
 
 if __name__ == "__main__":
     print(sum_(10, 20))
